97ad74b751f34be04f327fc89a7cd2e080da09c1
MartaM096 <martam096@wp.pl>
2022-09-20 18:05:04 +0200

    added string name and surname added int age

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter your name");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner
+        String name = scanner.next();
+        System.out.println("Enter your surname");
+        String surname = scanner.next();
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
     }
 }
